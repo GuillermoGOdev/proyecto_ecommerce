2623f5a09040a94f8d67958cd7687422e5cd6570
YixSoviet <69943781+Yix-Soviet@users.noreply.github.com>
2026-07-22 10:59:22 -0500

    Add: Patron observador con implementacion en el inventario y notificacion por consola

--- backend/src/main/java/com/ecommerce/ecommerce_backend/service/impl/InventarioServiceImpl.java
+++ backend/src/main/java/com/ecommerce/ecommerce_backend/service/impl/InventarioServiceImpl.java
@@ -2,6 +2,8 @@ package com.ecommerce.ecommerce_backend.service.impl;
 
 import com.ecommerce.ecommerce_backend.model.MovimientoInventario;
 import com.ecommerce.ecommerce_backend.model.Producto;
+import com.ecommerce.ecommerce_backend.observer.InventarioSubject;
+import com.ecommerce.ecommerce_backend.observer.NotificacionInventarioObserver;
 import com.ecommerce.ecommerce_backend.repository.MovimientoRepository;
 import com.ecommerce.ecommerce_backend.repository.ProductoRepository;
 import com.ecommerce.ecommerce_backend.service.IInventarioService;
@@ -19,6 +21,11 @@ public class InventarioServiceImpl implements IInventarioService {
     private MovimientoRepository movimientoRepository;
     @Autowired
     private ProductoRepository productoRepository;
+    private  final InventarioSubject inventarioSubject = new InventarioSubject();
+
+    public InventarioServiceImpl() {
+        inventarioSubject.agregarObservador(new NotificacionInventarioObserver());
+    }
 
     @Override
     @Transactional 
@@ -39,6 +46,7 @@ public class InventarioServiceImpl implements IInventarioService {
         productoRepository.save(producto);
         MovimientoInventario movimiento = new MovimientoInventario(producto, cantidad, tipo, observacion);
         movimientoRepository.save(movimiento);
+        inventarioSubject.notificar(movimiento);
     }
 
     @Override
